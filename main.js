f6dd078316431bc1e435882038add68ccff5762b
Evgenyj <worker_lab@mail.ru>
2021-12-26 19:55:16 +0300

    JS-Sunday-3-progress

--- main.js
+++ main.js
@@ -6,12 +6,16 @@
 
 const small = document.getElementById("small")
 
-const onClickHandler = () => {
+const onClickHandler = (e) => {
     alert()
 }
 
-small.onclick = onClickHandler // способ ниже отличается тем, что на обработчик ниже можно повесить больше событий
-small.onclick = null
-/*
-small.addEventListener("click", () => { alert("dsfsf")})
-small.addEventListener("click", () => { alert("weddf")})*/
+/*small.onclick = onClickHandler // способ ниже отличается тем, что на обработчик ниже можно повесить больше событий
+small.onclick = null*/
+
+/*const handler = () => { alert("dsfsf")}
+// small.addEventListener("click", () => { alert("dsfsf")})
+small.addEventListener("click", handler)
+small.removeEventListener("click", handler)*/
+
+console.log(typeof (() => {}) === function)
