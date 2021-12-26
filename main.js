7385e9e574a9cec13ccbd1a735dcc9b7d7b775ea
Evgenyj <worker_lab@mail.ru>
2021-12-26 20:25:02 +0300

    JS-Sunday-3-progress-2

--- main.js
+++ main.js
@@ -5,17 +5,24 @@
 
 
 const small = document.getElementById("small")
+const med = document.getElementById("medium")
+const big = document.getElementById("big")
 
 const onClickHandler = (e) => {
-    alert()
+    // console.dir(e.target.id)
+    if(e.currentTarget.id === "small") {
+        console.dir(e.currentTarget.id)
+    }
 }
 
-/*small.onclick = onClickHandler // способ ниже отличается тем, что на обработчик ниже можно повесить больше событий
-small.onclick = null*/
+small.onclick = onClickHandler // способ ниже отличается тем, что на обработчик ниже можно повесить больше событий
+/*med.onclick = onClickHandler
+big.onclick = onClickHandler*/
+//small.onclick = null*/
 
 /*const handler = () => { alert("dsfsf")}
 // small.addEventListener("click", () => { alert("dsfsf")})
 small.addEventListener("click", handler)
 small.removeEventListener("click", handler)*/
 
-console.log(typeof (() => {}) === function)
+// console.log(typeof ((() => {})) === "function")
