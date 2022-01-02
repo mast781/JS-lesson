20bbc07eacc96ee70b1532a86cd6f9336fd78535
Evgenyj <worker_lab@mail.ru>
2022-01-02 22:37:46 +0300

    Js-lessons-4-final + reduce

--- main.js
+++ main.js
@@ -38,11 +38,11 @@ const tasks = {
     ]
 }
 
-console.log(tasks[todoListID_1])
-console.log(tasks[todoListID_1].filter(t => t.title === "HTML"))
-console.log([...tasks[todoListID_1]])
-console.log([...tasks[todoListID_1]] === tasks[todoListID_1])
-console.log([...tasks[todoListID_1], {id: "03lk", title: "TS", isDone: true}])
+// console.log(tasks[todoListID_1])
+// console.log(tasks[todoListID_1].filter(t => t.title === "HTML"))
+// console.log([...tasks[todoListID_1]])
+// console.log([...tasks[todoListID_1]] === tasks[todoListID_1])
+// console.log([...tasks[todoListID_1], {id: "03lk", title: "TS", isDone: true}])
 
 console.log({
     ...tasks,
@@ -53,3 +53,49 @@ console.log({
 })
 
 console.log(todoLists)
+
+// reduce
+
+const nums = [1, 2, 3, 4, 5]
+console.log(nums.reduce((sum, el) => sum + el, 0))
+
+console.log(nums.reduce((max,el)=> max > el ? max : el))
+
+let students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    }
+];
+
+console.log(students.reduce((sum, st) => sum + st.scores, 0))
+console.log(students.reduce((best, st) => best.scores > st.scores ? best : st))
+
+console.log(students.reduce((arr, st) => {
+    return arr.concat({...st, scores: st.scores + 10})
+}, []))
+
+console.log(students.reduce((data, st) => {
+    data[st.name] = [st.age, st.scores, st.isMarried]
+    return data
+}, {}))
