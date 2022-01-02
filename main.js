f09f33b854b1bdfcd8f51c35278ba59c73077804
Evgenyj <worker_lab@mail.ru>
2022-01-02 21:05:27 +0300

    Js-lessons-4-progress

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+const todoListID_1 = "23hg-87kf"
+const todoListID_2 = "89hf-02lj"
+
+const todoLists = [
+    {
+        id: todoListID_1, //"23hg-87kf",
+        title: "What to learn",
+        filter: "all",
+        /*tasks: [
+            {id:"57hf",title:"HTML", isDone: true},
+            {id:"68kl",title:"CSS", isDone: true},
+            {id:"03lk",title:"REACT", isDone: true},
+        ]*/
+    },
+    {
+        id: todoListID_2,//"89hf-02lj",
+        title: "What to buy",
+        filter: "all",
+        /*tasks: [
+            {id:"11hf",title:"Meat", isDone: true},
+            {id:"22kl",title:"Bear", isDone: true},
+            {id:"33lk",title:"Milk", isDone: true},
+        ]*/
+    },
+]
 
 
-console.log("yo!!!")
+const tasks = {
+    "23hg-87kf": [
+        {id: "57hf", title: "HTML", isDone: true},
+        {id: "68kl", title: "CSS", isDone: true},
+        {id: "03lk", title: "REACT", isDone: true},
+    ],
+    "89hf-02lj": [
+        {id: "11hf", title: "Meat", isDone: true},
+        {id: "22kl", title: "Bear", isDone: true},
+        {id: "33lk", title: "Milk", isDone: true},
+    ]
+}
